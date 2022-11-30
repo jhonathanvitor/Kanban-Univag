5f6fa9a99828ef616db5e80acdd9d860f468b113
jhonathan vitor <jhonathanvitor@gmail.com>
2022-11-30 11:23:07 -0400

    env local

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'https://kanban-univag-bns9t1opb-jhonathanvitor.vercel.app/api/v1/'
+const baseUrl = (process.env.REACT_APP_URI)
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
